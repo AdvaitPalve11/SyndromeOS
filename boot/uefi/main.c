d2c3fc4b0b53760c589b536c8ebb695d20d08952
Advait Palve <advaitdpalve@gmail.com>
2026-09-05 01:20:00 +0530

    Update UEFI entry point to display logo on boot

--- boot/uefi/main.c
+++ boot/uefi/main.c
@@ -1,29 +1,24 @@
 #include <efi.h>
 #include <efilib.h>
+#include "../graphics/graphics.h"
 
 EFI_STATUS EFIAPI efi_main(
     EFI_HANDLE ImageHandle,
     EFI_SYSTEM_TABLE *SystemTable
 ) {
     InitializeLib(ImageHandle, SystemTable);
+    GraphicsDrawLogo(SystemTable);
 
     Print(L"\r\n");
-    Print(L"========================================\r\n");
-    Print(L"              SyndromeOS\r\n");
-    Print(L"========================================\r\n");
-    Print(L"\r\n");
-
-    Print(L"SyndromeOS UEFI Bootloader\r\n");
-    Print(L"Boot successful.\r\n");
-    Print(L"\r\n");
-
     Print(L"Architecture : x86_64\r\n");
     Print(L"Boot mode    : UEFI\r\n");
     Print(L"\r\n");
-
     Print(L"Press any key to exit...\r\n");
 
-    WaitForSingleEvent( SystemTable->ConIn->WaitForKey, 0);
+    WaitForSingleEvent(
+        SystemTable->ConIn->WaitForKey,
+        0
+    );
 
     return EFI_SUCCESS;
 }
